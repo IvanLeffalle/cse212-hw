5d088c1fb620ee8341c18826c39f944eff3e83d1
Ivan Leffalle <ivanmleffalle@gmail.com>
2025-11-18 01:12:27 -0300

    W03 Code: Sets and Maps

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -32,7 +32,13 @@ public class Maze
     /// </summary>
     public void MoveLeft()
     {
-        // FILL IN CODE
+        var key = (_currX, _currY);
+
+        if (!_mazeMap[key][0])
+            throw new InvalidOperationException("Can't go that way!");
+
+        _currX -= 1;
+
     }
 
     /// <summary>
@@ -41,7 +47,12 @@ public class Maze
     /// </summary>
     public void MoveRight()
     {
-        // FILL IN CODE
+        var key = (_currX, _currY);
+
+        if (!_mazeMap[key][1])
+            throw new InvalidOperationException("Can't go that way!");
+
+        _currX += 1;
     }
 
     /// <summary>
@@ -51,6 +62,12 @@ public class Maze
     public void MoveUp()
     {
         // FILL IN CODE
+        var key = (_currX, _currY);
+
+        if (!_mazeMap[key][2])
+            throw new InvalidOperationException("Can't go that way!");
+
+        _currY -= 1;
     }
 
     /// <summary>
@@ -60,6 +77,12 @@ public class Maze
     public void MoveDown()
     {
         // FILL IN CODE
+        var key = (_currX, _currY);
+
+        if (!_mazeMap[key][3])
+            throw new InvalidOperationException("Can't go that way!");
+
+        _currY += 1;
     }
 
     public string GetStatus()
